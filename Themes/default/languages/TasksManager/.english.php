ca916e1bf1d3946984e732f80b871f0b57b673f8
Diego Andrés <diegoandres_cortes@outlook.com>
2022-02-09 23:48:24 -0600

    add language wip
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Themes/default/languages/TasksManager/.english.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * @package Tasks Manager
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2022, SMF Tricks
+ * @license MIT
+ */
+
+ // Main Strings
+$txt['TasksManager_button'] = 'Tasks Manager';
+$txt['TasksManager_list'] = 'List';
+
+// Tasks and projects
+$txt['TasksManager_tasks'] = 'Tasks';
+$txt['TasksManager_projects'] = 'Projects';
+$txt['TasksManager_tp_all'] = 'All ';
+$txt['TasksManager_add_project'] = 'Add project';
+$txt['TasksManager_edit_project'] = 'Edit project';
+$txt['TasksManager_add_task'] = 'Add task';
+$txt['TasksManager_edit_task'] = 'Edit task';
+
+// Categories
+$txt['TasksManager_categories'] = 'Categories';
+$txt['TasksManager_categories_desc'] = 'Here you can see a list of categories and manage them accordingly.';
+$txt['TasksManager_add_category'] = 'Add category';
+$txt['TasksManager_add_category_desc'] = 'Here you can add a new category.';
+$txt['TasksManager_edit_category'] = 'Edit category';
+$txt['TasksManager_edit_category_desc'] = 'Edit the name of the category.';
+$txt['TasksManager_editp_category'] = $txt['TasksManager_edit_category'];
+$txt['TasksManager_editp_category_desc'] = $txt['TasksManager_edit_category_desc'];
+$txt['TasksManager_projects_cat'] = 'Projects';
+$txt['TasksManager_projects_cat_desc'] = 'Here you can see the list of categories for the projects.';
+$txt['TasksManager_tasks_cat'] = 'Tasks';
+$txt['TasksManager_tasks_cat_desc'] = 'Here you can see the list of categories for the tasks.';
+$txt['TasksManager_category_name'] = 'Category name';
+$txt['TasksManager_category_type'] = 'Category type';
+$txt['TasksManager_category_deleted'] = 'Category deleted';
+$txt['TasksManager_category_updated'] = 'Category updated';
+$txt['TasksManager_category_added'] = 'Category added';
+
+// Statuses
+$txt['TasksManager_statuses'] = 'Statuses';
+$txt['TasksManager_status_list'] = 'List';
+$txt['TasksManager_status_list_desc'] = 'Here you can see a list of statuses and manage them accordingly.';
+$txt['TasksManager_status_add'] = 'Add status';
+$txt['TasksManager_status_add_desc'] = 'Here you can add a new status.';
+$txt['TasksManager_status_delete'] = 'Delete status';
+$txt['TasksManager_status_name'] = 'Status name';
+$txt['TasksManager_edit_status'] = 'Edit status';
+$txt['TasksManager_edit_status_desc'] = 'Edit the name of the status.';
+$txt['TasksManager_add_status'] = 'Add status';
+$txt['TasksManager_add_status_desc'] = 'Here you can add a new status.';
+$txt['TasksManager_status_updated'] = 'Status updated';
+$txt['TasksManager_status_added'] = 'Status added';
+$txt['TasksManager_status_deleted'] = 'Status deleted';
+
+// Project Types
+$txt['TasksManager_types'] = 'Project Types';
+$txt['TasksManager_types_index'] = 'Index';
+$txt['TasksManager_types_index_desc'] = 'Here you can see a list of the project types available.';
+$txt['TasksManager_add_type'] = 'Add type';
+$txt['TasksManager_add_type_desc'] = 'Here you can add a new project type.';
+$txt['TasksManager_edit_type'] = 'Edit type';
+$txt['TasksManager_edit_type_desc'] = 'Edit the name of the project type.';
+$txt['TasksManger_types_name'] = 'Type name';
+$txt['TasksManger_types_deleted'] = 'Type deleted';
+$txt['TasksManger_types_updated'] = 'Type updated';
+$txt['TasksManger_types_added'] = 'Type added';
+
+// Configuration
+$txt['TasksManager_config'] = 'Configuration';
+$txt['TasksManager_config_desc'] = 'Here you can configure the Tasks Manager settings.';
+$txt['TasksManager_modify'] = 'Modify';
+$txt['TasksManager_manage'] = 'Manage';
+
+// Settings
+$txt['tppm_title'] = 'Button Title';
+$txt['tppm_title_desc'] = 'The title of the button. <span class="smalltext">(Default: Tasks Manager)</span>';
+$txt['tppm_items_per_page'] = 'Items per page';
+$txt['tppm_items_per_page_desc'] = 'The number of items per page (categories, statuses, etc). <span class="smalltext">(Default: 20)</span>';
+
+ // Permissions
+$txt['TasksManager_permissions'] = 'Permissions';
+$txt['permissiongroup_tasksmanager'] = 'Tasks Manager Permissions';
+$txt['permissionname_tasksmanager_can_view'] = 'View Tasks Manager';
+$txt['groups_tasksmanager_can_view'] = 'Tasks Manager View';
+$txt['permissionhelp_tasksmanager_can_view'] = 'This permission allows users to view the Tasks and Projects.';
+$txt['cannot_tasksmanager_can_view'] = 'Sorry, you are not allowed to view the Tasks and Projects.';
+$txt['permissionname_tasksmanager_can_edit'] = 'Edit Tasks Manager';
+$txt['groups_tasksmanager_can_edit'] = 'Tasks Manager Edit';
+$txt['permissionhelp_tasksmanager_can_edit'] = 'This permission allows users to edit and manage the Tasks and Projects.';
+$txt['cannot_tasksmanager_can_edit'] = 'Sorry, you are not allowed to edit Tasks and Projects.';
+
+// Errors
+$txt['TasksManager_no_project'] = 'No project selected.';
+$txt['TasksManager_no_task'] = 'No task selected.';
+$txt['TasksManager_no_category'] = 'No category selected.';
+$txt['TasksManager_no_category_name'] = 'No category name entered.';
+$txt['TasksManager_no_categories'] = 'No categories have been added.';
+$txt['TasksManager_no_statuses'] = 'No statuses have been added.';
+$txt['TasksManager_no_status'] = 'No status selected.';
+$txt['TasksManager_no_status_name'] = 'No status name entered.';
+$txt['TasksManager_no_types'] = 'No types have been added.';
+$txt['TasksManager_no_type'] = 'No type selected.';
+$txt['TasksManager_no_type_name'] = 'No type name entered.';
