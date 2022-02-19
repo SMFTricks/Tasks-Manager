2e63975c055db1d3204ba0efcea0c7634fdbea15
Diego Andrés <diegoandres_cortes@outlook.com>
2022-02-18 21:22:22 -0600

    add selectors and some fixes
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Themes/default/TasksManager.template.php
+++ Themes/default/TasksManager.template.php
@@ -45,11 +45,16 @@ function template_manage()
 	global $txt, $context;
 
 	echo '
-	<div class="windowbg">
+	<div class="windowbg">';
+
+	// Settings
+	if (!empty($context['tasks_pp_settings']))
+	{
+		echo '
 		<form action="', $context['post_url'] , '" method="post">
 			<dl class="settings">';
 
-		// Settings
+		// Display the settings
 		foreach ($context['tasks_pp_settings'] as $name => $setting)
 		{
 			if (!empty($setting['label']))
@@ -84,19 +89,19 @@ function template_manage()
 					<textarea name="', $name, '" id="tasks_', $name, '">', isset($setting['value']) ? $setting['value'] : '', '</textarea>';
 			}
 
-			// Time
-			elseif ($setting['type']  == 'time')
+			// Date picker using SMF's
+			elseif ($setting['type']  == 'date')
 			{
 				echo '
 					<div class="event_options_left" id="event_time_input">
-						<input type="text" name="', $name, '" id="tasks_', $name, '" maxlength="10" value="', $setting['value'], '" tabindex="', $context['tabindex']++, '" class="date_input start" data-type="date">
+						<input type="text" name="', $name, '" id="tasks_', $name, '" maxlength="10" value="', isset($setting['value']) ? $setting['value'] : '', '" tabindex="', $context['tabindex']++, '" class="date_input start" data-type="date">
 					</div>';
 			}
 				
 			// Text, number, etc
 			else
 				echo '
-					<input type="', $setting['type'], '" id="tasks_', $name, '" name="', $name, '" value="', $setting['value'], '"', (!isset($setting['size']) ? '' : 'size="' . $setting['size'] . '"'), ' />';
+					<input type="', $setting['type'], '" id="tasks_', $name, '" name="', $name, '" value="', isset($setting['value']) ? $setting['value'] : '', '"', (!isset($setting['size']) ? '' : ' size="' . $setting['size'] . '"'), (!isset($setting['max']) ? '' : ' max="' . $setting['max'] . '"'), ' />';
 
 			if (!empty($setting['label']))
 				echo '
@@ -109,6 +114,135 @@ function template_manage()
 		echo '
 			</dl>
 			<button class="button floatright">', $txt['save'], '</button>
-		</form>
+		</form>';
+	}
+
+	else
+		echo $txt['TasksManager_no_settings'];
+
+	echo '
 	</div>';
 }
+
+function template_add_topic_above()
+{
+	global $context, $scripturl;
+
+	echo '
+		<div class="noticebox">
+			', $context['TasksManager_adding_topic'], '
+			<a href="', $scripturl, '?topic=', $_REQUEST['id'] . '.0">
+				', $context['task_topic_subject'], '
+			</a>
+		</div>';
+}
+
+function template_add_topic_below() {}
+
+function template_list_selector_above()
+{
+	global $context, $txt;
+
+	if (!empty($context['form_url']))
+	{
+		echo '
+		<form action="', $context['form_url'], '" method="post" class="floatright" style="margin: 0 auto 5px">
+			', $txt['TasksManager_tp_filter'], ': ';
+
+		// Categories
+		if (!empty($context['tasks_category_list']))
+		{
+			echo '
+			<select name="category" onchange="submit();">
+				<optgroup label="'. $txt['TasksManager_categories']. '">
+					<option value="-1"'. (!isset($_REQUEST['category']) || $_REQUEST['category'] == -1 ? ' selected="selected"' : ''). '>'. $txt['TasksManager_tp_all']. '</option>
+					<option value="0"'. (isset($_REQUEST['category']) && $_REQUEST['category'] == 0 ? ' selected="selected"' : ''). '>'. $txt['TasksManager_projects_no_category']. '</option>';
+
+			foreach ($context['tasks_category_list'] as $category)
+				echo '
+					<option value="'. $category['category_id'] .'"'. (isset($_REQUEST['category']) && $_REQUEST['category'] == $category['category_id'] ? ' selected="selected"' : ''). '>'. $category['category_name']. '</option>';
+
+			echo '
+				</optgroup>
+			</select>';
+		}
+
+		// Status
+		if (!empty($context['tasks_status_list']))
+		{
+			echo '
+			<select name="status" onchange="submit();">
+				<optgroup label="'. $txt['TasksManager_status']. '">
+					<option value="-1"'. (!isset($_REQUEST['status']) || $_REQUEST['status'] == -1 ? ' selected="selected"' : ''). '>'. $txt['TasksManager_tp_all']. '</option>
+					<option value="0"'. (isset($_REQUEST['status']) && $_REQUEST['status'] == 0 ? ' selected="selected"' : ''). '>'. $txt['TasksManager_projects_no_status']. '</option>';
+
+			foreach ($context['tasks_status_list'] as $status)
+				echo '
+					<option value="', $status['status_id'], '"', (isset($_REQUEST['status']) && $_REQUEST['status'] == $status['status_id'] ? ' selected="selected"' : ''), '>', $status['status_name'], '</option>';
+
+			echo '
+				</optgroup>
+			</select>';
+		}
+
+		// Types
+		if (!empty($context['tasks_type_list']))
+		{
+			echo '
+			<select name="type" onchange="submit();">
+				<optgroup label="'. $txt['TasksManager_types']. '">
+					<option value="-1"'. (!isset($_REQUEST['type']) || $_REQUEST['type'] == -1 ? ' selected="selected"' : ''). '>'. $txt['TasksManager_tp_all']. '</option>
+					<option value="0"'. (isset($_REQUEST['type']) && $_REQUEST['type'] == 0 ? ' selected="selected"' : ''). '>'. $txt['TasksManager_projects_no_type']. '</option>';
+
+			foreach ($context['tasks_type_list'] as $type)
+				echo '
+					<option value="', $type['type_id'], '"', (isset($_REQUEST['type']) && $_REQUEST['type'] == $type['type_id'] ? ' selected="selected"' : ''), '>', $type['type_name'], '</option>';
+
+			echo '
+				</optgroup>
+			</select>';
+		}
+
+		// Projects
+		if (!empty($context['tasks_projects_list']))
+		{
+			echo '
+			<select name="project" onchange="submit();">
+				<optgroup label="'. $txt['TasksManager_projects']. '">
+					<option value="-1"'. (!isset($_REQUEST['project']) || $_REQUEST['project'] == -1 ? ' selected="selected"' : ''). '>'. $txt['TasksManager_tp_all']. '</option>
+					<option value="0"'. (isset($_REQUEST['project']) && $_REQUEST['project'] == 0 ? ' selected="selected"' : ''). '>'. $txt['TasksManager_tasks_no_project']. '</option>';
+
+			foreach ($context['tasks_projects_list'] as $project)
+				echo '
+					<option value="', $project['project_id'], '"'. (isset($_REQUEST['project']) && $_REQUEST['project'] == $project['project_id'] ? ' selected="selected"' : ''). '>', $project['project_title'], '</option>';
+
+			echo  '
+				</optgroup>
+			</select>';
+		}
+
+		// Tasks
+		if (!empty($context['tasks_tasks_list']))
+		{
+			echo '
+			<select name="task" onchange="submit();">
+				<optgroup label="'. $txt['TasksManager_tasks']. '">
+					<option value="-1"'. (!isset($_REQUEST['task']) || $_REQUEST['task'] == -1 ? ' selected="selected"' : ''). '>'. $txt['TasksManager_tp_all']. '</option>';
+
+			foreach ($context['tasks_tasks_list'] as $task)
+				echo '
+				<option value="', $task['task_id'], '">', $task['task_name'], '</option>';
+
+			echo  '
+				</optgroup>
+			</select>';
+		}
+
+		echo '
+			<button class="button floatright">', $txt['go'], '</button>
+		</form>
+		<br class="clear" />';
+	}
+}
+
+function template_list_selector_below() {}
