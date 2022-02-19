4e8db6832ddd2673e3648ee15d3a94c8f247219b
Diego Andrés <diegoandres_cortes@outlook.com>
2022-02-18 21:23:23 -0600

    link tasks from topics
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Sources/TasksManager/Integration.php
+++ Sources/TasksManager/Integration.php
@@ -36,6 +36,7 @@ class Integration
 		add_integration_function('integrate_admin_areas', __CLASS__ . '::language', false);
 		add_integration_function('integrate_load_permissions', __CLASS__ . '::load_permissions#', false);
 		add_integration_function('integrate_load_illegal_guest_permissions', __CLASS__ . '::illegal_guest#', false);
+		add_integration_function('integrate_mod_buttons', __CLASS__ . '::mod_buttons#', false);
 	}
 
 	public static function autoload(&$classMap)
@@ -84,5 +85,29 @@ class Integration
 
 		// Guest should not be able to edit or add anything
 		$context['non_guest_permissions'][] = 'tasksmanager_can_edit';
+	}
+
+	public function mod_buttons(&$mod_buttons)
+	{
+		global $scripturl, $context, $txt;
+
+		// Don't do anything if we don't have the permission
+		if (!allowedTo('tasksmanager_can_edit'))
+			return;
+
+		// Find if this topic is in any task
+		if (($topic_task = cache_get_data('tasksmanager_topic_tasks_' . $context['current_topic'], 3600)) === null)
+		{
+			// Get tasks with this topic
+			$topic_task = Tasks::getTasks(0, 100000, 'tk.task_id', 'WHERE tk.topic_id = {int:topic}', ['topic' => $context['current_topic']]);
+
+			cache_put_data('tasksmanager_topic_tasks_' . $context['current_topic'], $topic_task, 3600);
+		}
+
+		// Add a topic to a task
+		if (empty($topic_task))
+			$mod_buttons['tasksmanager_add_task'] = ['text' => 'TasksManager_add_topic_task', 'icon' => 'tasksmanager', 'url' => $scripturl . '?action=tasksmanager;area=tasks;sa=addtopic;id=' . $context['current_topic'] . ';' . $context['session_var'] . '=' . $context['session_id']];
+		else
+			$mod_buttons['tasksmanager_remove_task'] = ['text' => 'TasksManager_remove_topic_task', 'icon' => 'delete', 'url' => $scripturl . '?action=tasksmanager;area=tasks;sa=deletetopic;id=' . $context['current_topic'] . ';' . $context['session_var'] . '=' . $context['session_id']];
 	}
 }
