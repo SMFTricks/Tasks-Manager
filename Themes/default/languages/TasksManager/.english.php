a82daf8467d210f964bec45c8eb1546ff713694f
Diego Andrés <diegoandres_cortes@outlook.com>
2022-02-18 21:22:35 -0600

    update language strings
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Themes/default/languages/TasksManager/.english.php
+++ Themes/default/languages/TasksManager/.english.php
@@ -14,6 +14,7 @@ $txt['TasksManager_list'] = 'List';
 
 // Tasks and projects
 $txt['TasksManager_tp_all'] = 'All ';
+$txt['TasksManager_tp_filter'] = 'Filter';
 
 // Projects
 $txt['TasksManager_projects'] = 'Projects';
@@ -27,6 +28,9 @@ $txt['TasksManager_projects_title'] = 'Project title';
 $txt['TasksManager_projects_picture'] = 'Project picture';
 $txt['TasksManager_projects_picture_desc'] = 'Add the link to an icon to use as the project picture. <br><em>Recommended size: 64x64px or smaller</em>';
 $txt['TasksManager_projects_description'] = 'Project description';
+$txt['TasksManager_projects_description_desc'] = 'Short description of the project, BBC allowed.';
+$txt['TasksManager_projects_additional_details'] = 'Additional details';
+$txt['TasksManager_projects_additional_details_desc'] = 'Additional details about the project, BBC allowed.';
 $txt['TasksManager_projects_category'] = 'Project category';
 $txt['TasksManager_projects_no_category'] = 'No category';
 $txt['TasksManager_projects_type'] = 'Project type';
@@ -39,6 +43,7 @@ $txt['TasksManager_projects_added'] = 'Project added';
 $txt['TasksManager_projects_updated'] = 'Project updated';
 $txt['TasksManager_projects_deleted'] = 'Project deleted';
 $txt['TasksManager_projects_details'] = 'Project details';
+$txt['TasksManager_projects_view_tasks'] = 'View tasks';
 
 // Tasks
 $txt['TasksManager_tasks'] = 'Tasks';
@@ -59,6 +64,9 @@ $txt['TasksManager_tasks_added'] = 'Task added';
 $txt['TasksManager_tasks_updated'] = 'Task updated';
 $txt['TasksManager_tasks_deleted'] = 'Task deleted';
 $txt['TasksManager_tasks_details'] = 'Task details';
+$txt['TasksManager_tasks_time_booked'] = 'Time booked';
+$txt['TasksManager_tasks_estimated_hours'] = 'Estimated hours';
+$txt['TasksManager_tasks_estimated_unknown'] = 'Unknown';
 
 // Categories
 $txt['TasksManager_categories'] = 'Categories';
@@ -84,6 +92,7 @@ $txt['TasksManager_category_added'] = 'Category added';
 
 // Statuses
 $txt['TasksManager_statuses'] = 'Statuses';
+$txt['TasksManager_status'] = 'Status';
 $txt['TasksManager_status_list'] = 'List';
 $txt['TasksManager_status_list_desc'] = 'Here you can see a list of statuses and manage them accordingly.';
 $txt['TasksManager_status_add'] = 'Add status';
@@ -111,6 +120,26 @@ $txt['TasksManger_types_deleted'] = 'Type deleted';
 $txt['TasksManger_types_updated'] = 'Type updated';
 $txt['TasksManger_types_added'] = 'Type added';
 
+// Booking
+$txt['TasksManager_booking'] = 'Booking';
+$txt['TasksManager_booking_log'] = 'Time Log';
+$txt['TasksManager_booking_log_desc'] = 'Here you can see the time log for the tasks.';
+$txt['TasksManager_booking_booktime'] = 'Book Time';
+$txt['TasksManager_booking_booktime_desc'] = 'Here you can book the time for the tasks.';
+$txt['TasksManager_booking_date'] = 'Booking Date';
+$txt['TasksManager_booking_time'] = 'Time';
+$txt['TasksManager_booking_time_hours'] = 'Hours';
+$txt['TasksManager_booking_time_minutes'] = 'Minutes';
+$txt['TasksManager_booking_added'] = 'Booking added';
+$txt['TasksManager_booking_deleted'] = 'Booking deleted';
+
+// Other options
+$txt['TasksManager_add_topic_task'] = 'Add to a Task';
+$txt['TasksManager_remove_topic_task'] = 'Remove from Task';
+$txt['TasksManager_add_topic_task_desc'] = 'Here you can add a topic to an existing task.';
+$txt['TasksManager_adding_topic_task'] = 'You are about to add the following topic to a task: ';
+$txt['TasksManager_adding_topic_project'] = 'You are about to add the following topic to a project: ';
+
 // Configuration
 $txt['TasksManager_config'] = 'Configuration';
 $txt['TasksManager_config_desc'] = 'Here you can configure the Tasks Manager settings.';
@@ -122,6 +151,12 @@ $txt['tppm_title'] = 'Button Title';
 $txt['tppm_title_desc'] = 'The title of the button. <span class="smalltext">(Default: Tasks Manager)</span>';
 $txt['tppm_items_per_page'] = 'Items per page';
 $txt['tppm_items_per_page_desc'] = 'The number of items per page (categories, statuses, etc). <span class="smalltext">(Default: 20)</span>';
+$txt['tppm_items_filter'] = 'Amount of items to show in the filters';
+$txt['tppm_items_filter_desc'] = 'The number of items to show in the filters (categories, statuses, etc). <span class="smalltext">(Default: 25)</span><br>Because of this, items are displayed based on ID and not name, it\'s meant to help you keep track of recent items, not all of them.<br>But since I know someone will ask to change it, you have an option below, happy?';
+$txt['tppm_filter_sort'] = 'Sort items in filters';
+$txt['tppm_filter_sort_desc'] = 'Sort items in filters by ID or name. <span class="smalltext">(Default: ID)</span>';
+$txt['tppm_filter_sort_name'] = 'Sort by Name';
+$txt['tppm_filter_sort_id'] = 'Sort by ID';
 
  // Permissions
 $txt['TasksManager_permissions'] = 'Permissions';
@@ -136,10 +171,12 @@ $txt['permissionhelp_tasksmanager_can_edit'] = 'This permission allows users to 
 $txt['cannot_tasksmanager_can_edit'] = 'Sorry, you are not allowed to edit Tasks and Projects.';
 
 // Errors
+$txt['TasksManager_no_settings'] = 'No settings found.';
 $txt['TasksManager_no_project'] = 'No project selected.';
 $txt['TasksManager_no_projects'] = 'No projects have been added.';
 $txt['TasksManager_no_task'] = 'No task selected.';
 $txt['TasksManager_no_tasks'] = 'No tasks have been added.';
+$txt['TasksManager_no_tasks_available'] = 'No tasks available.';
 $txt['TasksManager_no_category'] = 'No category selected.';
 $txt['TasksManager_no_category_name'] = 'No category name entered.';
 $txt['TasksManager_no_categories'] = 'No categories have been added.';
@@ -152,3 +189,8 @@ $txt['TasksManager_no_type_name'] = 'No type name entered.';
 $txt['TasksManager_no_project_title'] = 'No project title entered.';
 $txt['TasksManager_end_date_before_start_date'] = 'The end date must be after the start date.';
 $txt['TasksManager_no_task_name'] = 'No task name entered.';
+$txt['TasksManager_no_topic'] = 'No topic selected.';
+$txt['TasksManager_no_booking'] = 'No booking selected.';
+$txt['TasksManager_no_booking_time'] = 'No time entered.';
+$txt['TasksManager_no_time_worked'] = 'No time worked entered.';
+$txt['TasksManager_no_total_time'] = 'None';
